944029dbc8690c51196f92b6e76ee2139d498099
Gabriel Malheiros <gabrielmalheirosmf@hotmail.com>
2022-05-20 16:07:07 -0300

    segundocomit

--- fontes/help.js
+++ fontes/help.js
@@ -1,3 +1,3 @@
-git remote add origin https://github.com/GabrielMalheirosmf/21-do-Oscar.git
-git branch -M main
-git push -u origin main
+//git remote add origin https://github.com/GabrielMalheirosmf/21-do-Oscar.git
+//git branch -M main
+//git push -u origin main//
