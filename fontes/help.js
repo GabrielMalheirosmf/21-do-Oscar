f90e8a01ead16632ad8cf539ff202c6fdbf24ed4
Gabriel Malheiros <gabrielmalheirosmf@hotmail.com>
2022-06-04 14:46:22 -0300

    main

--- fontes/help.js
+++
@@ -1,3 +0,0 @@
-git remote add origin https://github.com/GabrielMalheirosmf/21-do-Oscar.git
-git branch -M main
-git push -u origin main
---
+++ script.js
@@ -0,0 +1,60 @@
+var botao1 = document.getElementById('btn1');
+    botao2 = document.getElementById('btn2');
+    botao3 = document.getElementById('btn3');
+    botao4 = document.getElementById('btn4');
+    botao5 = document.getElementById('btn5');
+    botao6 = document.getElementById('btn6');
+
+var num = 0;
+
+botao1.value = 1;
+botao2.value = 2;
+botao3.value = 3;
+botao4.value = 4;
+botao5.value = 5;
+botao6.value = 6;
+
+function butaoclick(click){
+    botao1.value = +click + 1;
+    botao2.value = +click + 2;
+    botao3.value = +click + 3;
+    botao4.value = +click + 4; 
+    botao5.value = +click + 5;
+    botao6.value = +click + 6;
+}
+botao1.onclick = () => {
+    num=botao1.value;
+    if(botao1.value == num && num == 21) alert("wel played");
+    if(botao1.value > num && num > 21) alert("bad played");
+    butaoclick(btn1.value)
+}
+botao2.onclick = () => {
+    num=botao2.value;
+    if(botao2.value == num && num == 21) alert("wel played");
+    if(botao2.value > num && num > 21) alert("bad played");
+    butaoclick(btn2.value)
+}
+botao3.onclick = () => {
+    num=botao3.value;
+    if(botao3.value == num && num == 21) alert("wel played");
+    if(botao3.value > num && num > 21) alert("bad played");
+    butaoclick(btn3.value)
+}
+botao4.onclick = () => {
+    num=botao4.value;
+    if(botao4.value == num && num == 21) alert("wel played");
+    if(botao4.value > num && num > 21) alert("bad played");
+    butaoclick(btn4.value)
+}
+botao5.onclick = () => {
+    num=botao5.value;
+    if(botao5.value == num && num == 21) alert("wel played");
+    if(botao5.value > num && num > 21) alert("bad played");
+    butaoclick(btn5.value)
+}
+botao6.onclick = () => {
+    num=botao6.value;
+    if(botao6.value == num && num == 21) alert("wel played");
+    if(botao6.value > num && num > 21) alert("bad played");
+    butaoclick(btn6.value)
+}
