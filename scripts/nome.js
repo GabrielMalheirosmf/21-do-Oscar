7b9dd1e53dc2beeb00f9b17b079a114018d4eb7f
Gabriel Malheiros <gabrielmalheirosmf@hotmail.com>
2022-06-10 16:22:00 -0300

    main

--- scripts/nome.js
+++ scripts/nome.js
@@ -1,24 +1,26 @@
 document.getElementById("submite").disabled = true;
+document.getElementById("input").addEventListener("input", function (event) {
 
-document.getElementById("input").addEventListener("input", function(event){
+  var conteudo = document.getElementById("input").value;
+  if (conteudo !== null && conteudo !== '') {
 
-
-    var conteudo = document.getElementById("input").value;
- 
-    if (conteudo !== null && conteudo !== '') {
-
-      document.getElementById("submite").disabled = false;
-    } else{
-      document.getElementById("submite").disabled = true;
-    }
+    document.getElementById("submite").disabled = false;
+  } else {
+    document.getElementById("submite").disabled = true;
+  }
 });
 
 var vnome;
-function gravar(){
-  vnome=document.getElementById("input").value;
-  localStorage.jogadorNome=vnome;
-}
-if(localStorage.jogadorNome){
-  vnome=localStorage.jogadorNome;
-  document.getElementById("painel").innerHTML= "bem vindo" + vnome;
-}
+var vnome2;
+  function gravar(){
+  vnome = document.getElementById("input").value;
+  localStorage.jogadorNome = vnome;
+  var storageLocal = localStorage.getItem('jogadornome')
+  document.body.querySelector("#painel").innerHTML = storageLocal;
+
+  vnome2 = document.getElementById("input1").value;
+  localStorage.jogadornome2 = vnome2 
+  var storageLocal2 = localStorage.getItem('jogadornome')
+  document.body.querySelector("#nome2").innerHTML = storageLocal2;
+  }
+
---
+++ scripts/scriptmulti.js
@@ -0,0 +1,124 @@
+
+var botao1 = document.getElementById('btn1');
+botao2 = document.getElementById('btn2');
+botao3 = document.getElementById('btn3');
+botao4 = document.getElementById('btn4');
+botao5 = document.getElementById('btn5');
+botao6 = document.getElementById('btn6');
+
+var num = 0;
+
+botao1.value = 1;
+botao2.value = 2;
+botao3.value = 3;
+botao4.value = 4;
+botao5.value = 5;
+botao6.value = 6;
+
+function butaoclick(click) {
+    botao1.value = +click + 1;
+    botao2.value = +click + 2;
+    botao3.value = +click + 3;
+    botao4.value = +click + 1;
+    botao5.value = +click + 2;
+    botao6.value = +click + 3;
+}
+function limpar() {
+    botao1.disabled = true;
+    botao2.disabled = true;
+    botao3.disabled = true;
+}
+function limpar2() {
+    botao4.disabled = true;
+    botao5.disabled = true;
+    botao6.disabled = true;
+}
+function add() {
+    botao1.disabled = false;
+    botao2.disabled = false;
+    botao3.disabled = false;
+}
+function add2() {
+    botao4.disabled = false;
+    botao5.disabled = false;
+    botao6.disabled = false;
+}
+botao1.onclick = () => {
+    num = botao1.value;
+    limpar()
+    add2()
+    if (botao1.value == num && num == 21){
+        window.location.href = 'vitoria.html';
+    }
+
+    if (botao1.value > num && num > 21) {
+        window.location.href = 'derrota.html';
+    }
+
+    butaoclick(btn1.value)
+}
+botao2.onclick = () => {
+    limpar()
+    add2()
+    num = botao2.value;
+    if (botao2.value == num && num == 21){
+        window.location.href = 'vitoria.html';
+    }
+    if (botao2.value > num && num > 21){
+        window.location.href = 'derrota.html';
+    }
+    butaoclick(btn2.value)
+}
+botao3.onclick = () => {
+    limpar()
+    add2()
+    num = botao3.value;
+    if (botao3.value == num && num == 21){
+        window.location.href = 'vitoria.html';
+    }
+    if (botao3.value > num && num > 21) {
+        window.location.href = 'derrota.html';
+    }
+    butaoclick(btn3.value)
+}
+botao4.onclick = () => {
+    num = botao4.value;
+    limpar2()
+    add()
+    if (botao4.value == num && num == 21) {
+        window.location.href = 'vitoria.html';
+    }
+
+    if (botao4.value > num && num > 21) {
+        window.location.href = 'derrota.html';
+    }
+    butaoclick(btn4.value)
+}
+botao5.onclick = () => {
+    num = botao5.value;
+    limpar2()
+    add()
+    if (botao5.value == num && num == 21) {
+        window.location.href = 'vitoria.html';
+    }
+
+    if (botao5.value > num && num > 21) {
+        window.location.href = 'derrota.html';
+    }
+    butaoclick(btn5.value)
+}
+botao6.onclick = () => {
+    num = botao6.value;
+    limpar2()
+    add()
+    if (botao6.value == num && num == 21) {
+        window.location.href = 'vitoria.html';
+    }
+
+    if (botao6.value > num && num > 21) {
+        window.location.href = 'derrota.html';
+    }
+    butaoclick(btn6.value)
+}
+
+
